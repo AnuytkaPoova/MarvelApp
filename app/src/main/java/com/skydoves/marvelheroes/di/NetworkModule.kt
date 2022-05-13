ddf5d46e7c0fe3d8cafc64435e1641e385955b53
APopova <a_ches@mail.ru>
2022-05-13 22:50:24 +0300

    home_2

---
+++ app/src/main/java/com/skydoves/marvelheroes/di/NetworkModule.kt
@@ -0,0 +1,36 @@
+
+
+package com.skydoves.marvelheroes.di
+
+import com.skydoves.marvelheroes.MarvelClient
+import com.skydoves.marvelheroes.MarvelService
+import com.skydoves.marvelheroes.RequestInterceptor
+import com.skydoves.marvelheroes.model.Poster
+import com.skydoves.sandwich.ResponseDataSource
+import okhttp3.OkHttpClient
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val networkModule = module {
+
+  single {
+    OkHttpClient.Builder()
+      .addInterceptor(RequestInterceptor())
+      .build()
+  }
+
+  single {
+    Retrofit.Builder()
+      .client(get<OkHttpClient>())
+      .baseUrl("https://gist.githubusercontent.com/skydoves/")
+      .addConverterFactory(GsonConverterFactory.create())
+      .build()
+  }
+
+  single { get<Retrofit>().create(MarvelService::class.java) }
+
+  single { MarvelClient(get()) }
+
+  single { ResponseDataSource<List<Poster>>() }
+}
---
+++ app/src/main/java/com/skydoves/marvelheroes/extensions/ActivityExtensions.kt
@@ -0,0 +1,10 @@
+
+
+package com.skydoves.marvelheroes.extensions
+
+import androidx.appcompat.app.AppCompatActivity
+import com.skydoves.transformationlayout.onTransformationEndContainer
+
+fun AppCompatActivity.onTransformationEndContainerApplyParams() {
+  onTransformationEndContainer(intent.getParcelableExtra("com.skydoves.transformationlayout"))
+}
